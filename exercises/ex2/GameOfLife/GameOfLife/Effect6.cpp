ed874bc7f7fb8061f176400e62056fa31fec7aa7
fcin <filip@cinik.eu>
2018-12-09 16:10:50 +0100

    Added effect 6

---
+++ exercises/ex2/GameOfLife/GameOfLife/Effect6.cpp
@@ -0,0 +1,48 @@
+#include "Effect6.h"
+
+Effect6::Effect6() { }
+
+void Effect6::apply(const Board ** boards, unsigned int x, unsigned int y, unsigned int dx, unsigned int dy, int boardId)
+{
+	if (boardId == 0 && m_BoardState1 == nullptr)
+		m_BoardState1 = new Board(boards[0]);
+	if (boardId == 1 && m_BoardState2 == nullptr)
+		m_BoardState1 = new Board(boards[1]);
+
+	if (m_BoardState1 == nullptr || m_BoardState2 == nullptr)
+		return;
+
+	int* cellsA = boards[0]->GetCells();
+	std::vector<int> toReplace;
+
+	for (size_t indexY = y; indexY <= dy; indexY++)
+	{
+		for (size_t indexX = x; indexX <= dx; indexX++)
+		{
+			size_t index = indexY * 16 + indexX;
+			if (cellsA[index] == 1)
+				toReplace.push_back(1);
+			else
+				toReplace.push_back(0);
+		}
+	}
+
+	int* cellsB = boards[1]->GetCells();
+	int curr = 0;
+
+	for (size_t indexY = y; indexY <= dy; indexY++)
+	{
+		for (size_t indexX = x; indexX <= dx; indexX++)
+		{
+			size_t index = indexY * 16 + indexX;
+			
+			cellsB[index] = toReplace[curr];
+			curr++;
+		}
+	}
+
+	if (boardId == 0)
+		delete m_BoardState1;
+	else
+		delete m_BoardState2;
+}
---
+++ exercises/ex2/GameOfLife/GameOfLife/Effect6.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Effect.h"
+#include "Board.h"
+#include <vector>
+
+class Effect6 : public Effect
+{
+private:
+	Board * m_BoardState1 = nullptr;
+	Board * m_BoardState2 = nullptr;
+
+public:
+	Effect6();
+	virtual void apply(const Board** boards, unsigned int x, unsigned int y, unsigned int dx, unsigned int dy, int boardId) override;
+};
